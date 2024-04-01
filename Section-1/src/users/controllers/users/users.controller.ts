5cc14a98a6bcc70bb39802f0b23fab1d947fe25e
Abrar Ali <abraralidev@gmail.com>
2024-04-01 16:14:50 +0500

    Posts Methods

--- Section-1/src/users/controllers/users/users.controller.ts
+++ Section-1/src/users/controllers/users/users.controller.ts
@@ -1,14 +1,61 @@
-import { Controller, Get, Post } from '@nestjs/common';
-import { title } from 'process';
+import { Controller, Get, Post, Req, Res, Body, Param, Query, UsePipes, ValidationPipe, ParseIntPipe, ParseBoolPipe } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { CreateUserDto } from './dtos/CreateUser.dto';
+
+
 
 @Controller('users')
 export class UsersController {
-
-    @Post()
-    createUser() {
-        return 'This action adds a new user';
+    //================================================================================================   
+    // Post API's
+    // create user by passing data in body according to nestjs example of passing data in body with validation
+    @Post('create')
+    @UsePipes(new ValidationPipe())
+    createUser(@Body() userData: CreateUserDto) {
+        console.log(userData.email);
+        return {
+            message: 'User created'
+        };
     }
+
+
+    // example of post request
+
+    // @Post('create')
+    // createUser(@Req() request: Request, @Res() response: Response) {
+    //     console.log(request.body);
+    //     response.send({ message: 'User created' });
+    // }
+
+    //================================================================================================   
     // Get API's
+
+    // get users by id according to nestjs example of passing id as a query parameter
+    @Get()
+    getUserById(@Query('sortDesc', ParseBoolPipe) sortDesc: boolean) {
+        console.log(sortDesc);
+        return {};
+    }
+
+
+    // get users by id according to nestjs example of passing id as a parameter
+    // @Get(':id')
+    // getUserById(@Param('id', ParseIntPipe) id: number) {
+    //     console.log(id);
+    //     return {
+    //         id: id,
+
+    //     };
+    // }
+
+    // get users by id according to express
+    // @Get(':id')
+    // getUserById(@Param() request: Request) {
+    //     console.log(request.params);
+    //     return {
+    //         id: request.params.id
+    //     };
+    // }
     // step one: create a controller with a route decorator and a method with a route decorator
     // step two: add a method to the controller with a route decorator
     // step three: return a simple object from the method
